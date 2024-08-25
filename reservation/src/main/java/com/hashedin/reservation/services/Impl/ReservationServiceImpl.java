f95f164bf25b3f6ec355a8190f0a9ca2cf5c5bd0
Raja <rajambr2002@gmail.com>
2024-08-25 19:30:58 +0530

    uncommited doc

--- reservation/src/main/java/com/hashedin/reservation/services/Impl/ReservationServiceImpl.java
+++ reservation/src/main/java/com/hashedin/reservation/services/Impl/ReservationServiceImpl.java
@@ -9,26 +9,46 @@ import com.hashedin.reservation.entity.Reservation;
 import com.hashedin.reservation.repository.ReservationRespository;
 import com.hashedin.reservation.services.ReservationService;
 
+/**
+ * Implementation class for the ReservationService interface.
+ */
 public class ReservationServiceImpl implements ReservationService{
 
     @Autowired
     private ReservationRespository reservationRepository;
 
+    /**
+     * Creates a new reservation.
+     * @param reservation The reservation to be created.
+     */
     @Override
     public void createReservation(Reservation reservation) {
         reservationRepository.save(reservation);
     }
 
+    /**
+     * Retrieves a reservation by its ID.
+     * @param id The ID of the reservation.
+     * @return The reservation with the specified ID.
+     */
     @Override
     public Reservation getReservationById(Long id) {
         return reservationRepository.findById(id).get();
     }
 
+    /**
+     * Retrieves all reservations.
+     * @return A list of all reservations.
+     */
     @Override
     public List<Reservation> getAllReservations() {
        return reservationRepository.findAll();
     }
 
+    /**
+     * Updates a reservation.
+     * @param reservation The updated reservation.
+     */
     @Override
     public void updateReservation(Reservation reservation) {
         Optional<Reservation> reservationData = reservationRepository.findById(reservation.getId());
@@ -42,9 +62,12 @@ public class ReservationServiceImpl implements ReservationService{
             reservationRepository.save(_reservation);
         }
         reservationRepository.save(reservation);
-        
     }
 
+    /**
+     * Deletes a reservation by its ID.
+     * @param id The ID of the reservation to be deleted.
+     */
     @Override
     public void deleteReservation(Long id) {
         reservationRepository.deleteById(id);        
